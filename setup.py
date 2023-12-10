608205376fa55a9413a219e6676eea3f52034a22
sharona19 <sharonarandiav@gmail.com>
2023-12-09 20:54:37 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='fixedincome',
+    version='0.1',
+    packages=['fixedincome'],
+    install_requires=[
+        # List your dependencies here
+    ],
+)
